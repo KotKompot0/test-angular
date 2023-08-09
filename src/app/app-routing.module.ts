20f533b151663f06feb02114e24110462fbfa107
Kotorkovsciy <kotorkovsciy@gmail.com>
2023-08-09 09:26:44 +0300

    feat: added routs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TimeComponent } from "./time/time.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: TimeComponent,
+  },
+  {
+    path: "**",
+    redirectTo: ""
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
